b00b6100efb3ad404c21b5e729316b6179ef8999
toyonori <gatyotyotyo@gmail.com>
2025-03-18 07:57:24 +0900

    render-build.shの修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Install dependencies
 bundle install
+
+# Check for yarn.lock and perform JS/CSS builds if present
+if [ -f yarn.lock ]; then
+  yarn install
+  yarn build
+  yarn build:css
+fi
+
+# Precompile assets
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+
+# Run migrations
 bundle exec rails db:migrate
