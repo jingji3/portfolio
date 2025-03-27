eea9d3651f675f1891beff1df2a0034b44d15a66
toyonori <gatyotyotyo@gmail.com>
2025-03-28 07:28:30 +0900

    ログイン・ログアウト機能#19

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   root "top#index"
   resources :users, only: %i[new create]
-  get 'login', to: 'user_session#new'
-  post 'login', to: 'user_session#create'
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
 end
