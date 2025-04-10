d0007d1b5ce65ae82ff746aa7cf81b156bb2ec63
toyonori <gatyotyotyo@gmail.com>
2025-04-10 21:42:36 +0900

    デプロイエラーの修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -14,8 +14,8 @@ if [ -f yarn.lock ]; then
 fi
 
 # アセットのPrecompile
-# bundle exec rails assets:precompile
-# bundle exec rails assets:clean
+ bundle exec rails assets:precompile
+ bundle exec rails assets:clean
 
 # migrations
 bundle exec rails db:migrate
