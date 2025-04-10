75e44b88d522894b3f93c29a15af3a863f62dc8e
toyonori <gatyotyotyo@gmail.com>
2025-04-10 20:56:02 +0900

    デプロイエラーの修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -11,11 +11,17 @@ if [ -f yarn.lock ]; then
   # 各アセットを独立してビルド
   yarn build:js
   yarn build:css || echo "Sass build failed, but continuing..."
+
+  mkdir -p public/assets
+  cp -f app/assets/builds/tailwind.css public/assets/
+  cp -f app/assets/builds/application.css public/assets/ 2>/dev/null || true
+  cp -f app/assets/builds/application.js public/assets/
+  cp -f app/assets/builds/theme_switcher.js public/assets/
 fi
 
 # アセットのPrecompile
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
+# bundle exec rails assets:precompile
+# bundle exec rails assets:clean
 
 # migrations
 bundle exec rails db:migrate
