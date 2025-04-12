45098c248dddb55fc59752b889a1f067244c2b67
toyonori <gatyotyotyo@gmail.com>
2025-04-12 15:15:13 +0900

    おすすめ動画のCURD

--- app/javascript/controllers/character_selector_controller.js
+++ app/javascript/controllers/character_selector_controller.js
@@ -4,7 +4,18 @@ export default class extends Controller {
   static targets = ["select", "image", "container"]
 
   connect() {
-    console.log("Character selector connected")
+    console.log("Character selector connected", this.element)
+
+    // デバッグログを追加
+    console.log("Select target:", this.selectTarget)
+    console.log("Select value:", this.selectTarget.value)
+    console.log("Data attribute:", this.selectTarget.dataset.characterSelectorInitialId)
+
+    // 初期値がある場合のみ処理
+    if (this.selectTarget.value && this.selectTarget.value !== '') {
+      console.log("Loading initial character from value:", this.selectTarget.value)
+      this.loadCharacterImage(this.selectTarget.value)
+    }
   }
 
   change(event) {
@@ -14,11 +25,27 @@ export default class extends Controller {
       return
     }
 
+    this.loadCharacterImage(characterId)
+  }
+
+  loadCharacterImage(characterId) {
+    console.log("Fetching character:", characterId)
     fetch(`/characters/${characterId}.json`)
-      .then(response => response.json())
+      .then(response => {
+        console.log("Response status:", response.status)
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`)
+        }
+        return response.json()
+      })
       .then(data => {
+        console.log("Character data received:", data)
         this.imageTarget.src = data.character_img
         this.containerTarget.classList.remove("hidden")
+      })
+      .catch(error => {
+        console.error("Error loading character image:", error)
+        this.containerTarget.classList.add("hidden")
       })
   }
 }
